d6731bc4b22b7628ca0851a14b2cb6abbe22164f
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-04-20 03:41:41 +0300

    Add Recipe initial view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,12 @@
 class RecipesController < ApplicationController
+  def index
+    @recipe = Recipe.all
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to recipe_path, status: :see_other
+  end
 end
